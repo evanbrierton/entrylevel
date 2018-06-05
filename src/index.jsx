e537827b4f47c63f6c8314f1898271293155a555
Evan Brierton <evanbrierton@icloud.com>
2018-06-05 19:26:04 +0100

    readd index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import firebase from 'firebase';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from './components/App';
+
+import config from './firebaseconfig';
+import registerServiceWorker from './registerServiceWorker';
+
+import './stylesheets/index.css';
+
+firebase.initializeApp(config);
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root'),
+);
+
+registerServiceWorker();
