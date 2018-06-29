f771ab6eb4d12220982ebb5aa7b68e08a11260ad
Evan Brierton <evanbrierton@icloud.com>
2018-06-29 19:59:53 +0100

    fix redux devtools bug

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,11 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 /* eslint-disable no-underscore-dangle */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  ),
+  composeEnhancers(applyMiddleware(thunk)),
 );
 /* eslint-enable */
 
